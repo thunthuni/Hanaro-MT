22786bbf0828b79e59dce19b0242071c29c727d2
Junsang Ryu <jjss52065@gmail.com>
2025-07-15 14:31:44 +0900

    - 데이터 변경

--- my_streamlit_app/modules/utils.py
+++ my_streamlit_app/modules/utils.py
@@ -3,8 +3,12 @@ import os
 import streamlit as st
 
 @st.cache_data
-def load_data(path="data/df_merged.xlsx"):
+def load_data(path="data/df_.xlsx"):
     base_dir = os.path.dirname(os.path.abspath(__file__))
     full_path = os.path.join(base_dir, "..", path)
     df = pd.read_excel(full_path)
     return df
+
+
+#def get_products_only(df):
+#    df.
