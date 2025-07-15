8f71bad820b9c37747c4856c18324c674bf7e686
Junsang Ryu <jjss52065@gmail.com>
2025-07-15 22:13:43 +0900

    - upload

--- my_streamlit_app/modules/utils.py
+++ my_streamlit_app/modules/utils.py
@@ -6,9 +6,19 @@ import streamlit as st
 def load_data(path="data/df_.xlsx"):
     base_dir = os.path.dirname(os.path.abspath(__file__))
     full_path = os.path.join(base_dir, "..", path)
-    df = pd.read_excel(full_path)
+    df = pd.read_excel(full_path, index_col=0)
     return df
 
 
-#def get_products_only(df):
-#    df.
+def get_products_df(df):
+    df["상품별해지율[%]"] = round(df['상품별해지율'] * 100, 2)
+    return df[['상품코드', "상품명", "상품구분", "상품별해지율[%]", "은행코드", "은행명"]].drop_duplicates().sort_values(by='상품별해지율[%]', ascending=False)
+
+
+
+
+
+"""
+df.drop(columns=["Acc_ID", "Contract_Date", "New_trsc_Amt", "Gender", "Age", "Job",	"Family", "Cancellation", "Card", 'Overdue', 
+                            'Unsubscribe', 'Telecom', 'Marketing', '연령대', 'Cancellation_bin']).drop_duplicates()
+"""
